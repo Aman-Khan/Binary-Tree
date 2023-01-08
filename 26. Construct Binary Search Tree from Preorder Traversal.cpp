e5c2201e0614e1a5cdf8918d85eb4d2db5e6cfab
dexter <49933542+Aman-Khan@users.noreply.github.com>
2023-01-08 16:12:06 +0530

    Create 26. Construct Binary Search Tree from Preorder Traversal.cpp

---
+++ 26. Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,24 @@
+    int findPostion(vector<int>& inorder, int ele){
+        int ind = -1;
+        for(int i=0; i<inorder.size(); i++){
+            if(inorder[i]==ele) return i;
+        }
+        return ind;
+    }
+    TreeNode* build(vector<int> &preorder, vector<int> &inorder, int &index, int ioStart, int ioEnd, int n){
+        if(index>=n || ioStart>ioEnd) return NULL;
+        int ele = preorder[index++];
+        TreeNode* root = new TreeNode(ele);
+        int position = findPostion(inorder, ele);
+        root->left = build(preorder, inorder, index, ioStart, position-1, n);
+        root->right = build(preorder, inorder, index, position+1, ioEnd, n);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        vector<int> inorder = preorder;
+        sort(inorder.begin(), inorder.end());
+        int index=0;
+        int n = preorder.size();
+        TreeNode* root = build(preorder, inorder, index, 0, n-1, n);
+        return root;
+    }
